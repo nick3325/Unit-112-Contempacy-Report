3b770d2a053d319b22b79ac579fd808546dee736
Nicholas Lucien <nicklucien@gmail.com>
2022-03-28 21:24:29 -0700

    First Commit

---
+++ posts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title 
